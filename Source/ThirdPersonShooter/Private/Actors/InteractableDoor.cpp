7b0988f86795d4650163777fd15f0cf9cedd509a
Danial Kama <dankam77.dk@gmail.com>
2022-12-12 18:37:45 +0330

    Create Interactable Door class

---
+++ Source/ThirdPersonShooter/Private/Actors/InteractableDoor.cpp
@@ -0,0 +1,10 @@
+// Copyright 2022 Danial Kamali. All Rights Reserved.
+
+#include "Actors/InteractableDoor.h"
+
+// Sets default values
+AInteractableDoor::AInteractableDoor()
+{
+	// Initialize variables
+	bIsEnable = false;
+}
---
+++ Source/ThirdPersonShooter/Private/Actors/InteractableDoor.h
@@ -0,0 +1,21 @@
+// Copyright 2022 Danial Kamali. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.h"
+#include "InteractableDoor.generated.h"
+
+UCLASS()
+class AInteractableDoor : public AInteractable
+{
+	GENERATED_BODY()
+
+// Functions
+public:
+	AInteractableDoor();
+
+// Variables
+public:
+	uint8 bIsEnable : 1;
+};

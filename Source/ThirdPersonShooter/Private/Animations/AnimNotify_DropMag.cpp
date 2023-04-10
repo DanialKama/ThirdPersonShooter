9aee1a4c45e0bc903f70ef21109adc48f2b1c64f
Danial Kama <dankam77.dk@gmail.com>
2023-04-10 17:44:16 +0330

    Modify UAnimNotify_DropMag

--- Source/ThirdPersonShooter/Private/Animations/AnimNotify_DropMag.cpp
+++ Source/ThirdPersonShooter/Private/Animations/AnimNotify_DropMag.cpp
@@ -1,10 +1,21 @@
 // Copyright 2022-2023 Danial Kamali. All Rights Reserved.
 
 #include "AnimNotify_DropMag.h"
+
 #include "Characters/BaseCharacter.h"
 
-void UAnimNotify_DropMag::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+UAnimNotify_DropMag::UAnimNotify_DropMag(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
+#if WITH_EDITORONLY_DATA
+	bShouldFireInEditor = false;
+	NotifyColor = FColor(162, 186, 209);
+#endif
+}
+
+void UAnimNotify_DropMag::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	
 	if (MeshComp && MeshComp->GetOwner())
 	{
 		if (ABaseCharacter* Character = Cast<ABaseCharacter>(MeshComp->GetOwner()))
--- Source/ThirdPersonShooter/Private/Animations/AnimNotify_DropMag.h
+++ Source/ThirdPersonShooter/Private/Animations/AnimNotify_DropMag.h
@@ -9,7 +9,7 @@
 UCLASS()
 class UAnimNotify_DropMag : public UAnimNotify
 {
-	GENERATED_BODY()
+	GENERATED_UCLASS_BODY()
 	
-	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
 };

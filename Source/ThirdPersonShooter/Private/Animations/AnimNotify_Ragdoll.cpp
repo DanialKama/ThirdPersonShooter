849b8c82975bb058ab00c26e92b0801b5bda0d6d
Danial Kama <dankam77.dk@gmail.com>
2023-04-10 17:44:55 +0330

    Modify UAnimNotify_Ragdoll

--- Source/ThirdPersonShooter/Private/Animations/AnimNotify_Ragdoll.cpp
+++ Source/ThirdPersonShooter/Private/Animations/AnimNotify_Ragdoll.cpp
@@ -1,10 +1,21 @@
 // Copyright 2022-2023 Danial Kamali. All Rights Reserved.
 
 #include "AnimNotify_Ragdoll.h"
+
 #include "Characters/BaseCharacter.h"
 
-void UAnimNotify_Ragdoll::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+UAnimNotify_Ragdoll::UAnimNotify_Ragdoll(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
+#if WITH_EDITORONLY_DATA
+	bShouldFireInEditor = false;
+	NotifyColor = FColor(156, 152, 148);
+#endif
+}
+
+void UAnimNotify_Ragdoll::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	
 	if (MeshComp && MeshComp->GetOwner())
 	{
 		if (ABaseCharacter* Character = Cast<ABaseCharacter>(MeshComp->GetOwner()))

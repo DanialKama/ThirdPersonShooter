3b78c0295bebbe778ca674d2504b88ce75bf5b1d
Danial Kama <dankam77.dk@gmail.com>
2023-04-10 17:46:05 +0330

    Modify UAnimNotifyState_Reload

--- Source/ThirdPersonShooter/Private/Animations/AnimNotifyState_Reload.cpp
+++ Source/ThirdPersonShooter/Private/Animations/AnimNotifyState_Reload.cpp
@@ -1,10 +1,21 @@
 // Copyright 2022-2023 Danial Kamali. All Rights Reserved.
 
 #include "AnimNotifyState_Reload.h"
+
 #include "Characters/BaseCharacter.h"
 
-void UAnimNotifyState_Reload::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration)
+UAnimNotifyState_Reload::UAnimNotifyState_Reload(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
+#if WITH_EDITORONLY_DATA
+	bShouldFireInEditor = false;
+	NotifyColor = FColor(166, 153, 142);
+#endif
+}
+
+void UAnimNotifyState_Reload::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+	
 	if (MeshComp && MeshComp->GetOwner())
 	{
 		if (ABaseCharacter* Character = Cast<ABaseCharacter>(MeshComp->GetOwner()))
@@ -14,8 +25,10 @@ void UAnimNotifyState_Reload::NotifyBegin(USkeletalMeshComponent* MeshComp, UAni
 	}
 }
 
-void UAnimNotifyState_Reload::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+void UAnimNotifyState_Reload::NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, const FAnimNotifyEventReference& EventReference)
 {
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+	
 	if (MeshComp && MeshComp->GetOwner())
 	{
 		if (ABaseCharacter* Character = Cast<ABaseCharacter>(MeshComp->GetOwner()))

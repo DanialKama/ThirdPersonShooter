685c8ed4d0b94c4b6656f736e8dabf5fe9079994
Danial Kama <dankam77.dk@gmail.com>
2023-04-10 17:45:23 +0330

    Modify UAnimNotify_RestoreHealth

--- Source/ThirdPersonShooter/Private/Animations/AnimNotify_RestoreHealth.cpp
+++ Source/ThirdPersonShooter/Private/Animations/AnimNotify_RestoreHealth.cpp
@@ -1,28 +1,42 @@
 // Copyright 2022-2023 Danial Kamali. All Rights Reserved.
 
 #include "AnimNotify_RestoreHealth.h"
+
 #include "AIController.h"
 #include "Characters/BaseCharacter.h"
 #include "Components/HealthComponent.h"
-#include "GameFramework/CharacterMovementComponent.h"
 #include "Core/Interfaces/AIControllerInterface.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
-void UAnimNotify_RestoreHealth::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+UAnimNotify_RestoreHealth::UAnimNotify_RestoreHealth(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
+#if WITH_EDITORONLY_DATA
+	bShouldFireInEditor = false;
+	NotifyColor = FColor(177, 188, 188);
+#endif
+}
+
+void UAnimNotify_RestoreHealth::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	
 	if (MeshComp && MeshComp->GetOwner())
 	{
 		if (const ABaseCharacter* Character = Cast<ABaseCharacter>(MeshComp->GetOwner()))
 		{
 			// Set movement mode to walking if it was set to null
 			// During AI healing movement set to null to stop AI from moving and interrupt montage
-			if (Character->GetCharacterMovement()->GetMovementName() == "NULL")
+			if (Character->GetCharacterMovement()->MovementMode == MOVE_None)
 			{
 				Character->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
 			}
+			
 			// Full health recovery
 			Character->GetHealthComponent()->Healing(Character->GetHealthComponent()->MaxHealth);
+			
 			// Set AI state if the owner is AI
 			AAIController* AIController = Cast<AAIController>(Character->GetController());
+			
 			if (AIController && AIController->GetClass()->ImplementsInterface(UAIControllerInterface::StaticClass()))
 			{
 				IAIControllerInterface::Execute_SetAIState(AIController, EAIState::Idle);

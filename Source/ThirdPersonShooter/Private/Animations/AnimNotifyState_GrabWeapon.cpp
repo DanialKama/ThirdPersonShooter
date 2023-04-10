4b8898d41d535b2cdef7ff8b34b0b39e82027d99
Danial Kama <dankam77.dk@gmail.com>
2023-04-10 17:45:45 +0330

    Modify UAnimNotifyState_GrabWeapon

--- Source/ThirdPersonShooter/Private/Animations/AnimNotifyState_GrabWeapon.cpp
+++ Source/ThirdPersonShooter/Private/Animations/AnimNotifyState_GrabWeapon.cpp
@@ -1,10 +1,21 @@
 // Copyright 2022-2023 Danial Kamali. All Rights Reserved.
 
 #include "AnimNotifyState_GrabWeapon.h"
+
 #include "Characters/BaseCharacter.h"
 
-void UAnimNotifyState_GrabWeapon::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration)
+UAnimNotifyState_GrabWeapon::UAnimNotifyState_GrabWeapon(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
+#if WITH_EDITORONLY_DATA
+	bShouldFireInEditor = false;
+	NotifyColor = FColor(171, 195, 164);
+#endif
+}
+
+void UAnimNotifyState_GrabWeapon::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+	
 	if (MeshComp && MeshComp->GetOwner())
 	{
 		if (ABaseCharacter* Character = Cast<ABaseCharacter>(MeshComp->GetOwner()))
@@ -13,8 +24,11 @@ void UAnimNotifyState_GrabWeapon::NotifyBegin(USkeletalMeshComponent* MeshComp, 
 		}
 	}
 }
-void UAnimNotifyState_GrabWeapon::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+
+void UAnimNotifyState_GrabWeapon::NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, const FAnimNotifyEventReference& EventReference)
 {
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+	
 	if (MeshComp && MeshComp->GetOwner())
 	{
 		if (ABaseCharacter* Character = Cast<ABaseCharacter>(MeshComp->GetOwner()))

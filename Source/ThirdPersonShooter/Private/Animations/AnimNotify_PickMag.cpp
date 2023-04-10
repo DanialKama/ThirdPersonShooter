5b0ec2d95603beb9b78f983f29863fcc1c98e665
Danial Kama <dankam77.dk@gmail.com>
2023-04-10 17:44:45 +0330

    Modify UAnimNotify_PickMag

--- Source/ThirdPersonShooter/Private/Animations/AnimNotify_PickMag.cpp
+++ Source/ThirdPersonShooter/Private/Animations/AnimNotify_PickMag.cpp
@@ -1,10 +1,21 @@
 // Copyright 2022-2023 Danial Kamali. All Rights Reserved.
 
 #include "AnimNotify_PickMag.h"
+
 #include "Characters/BaseCharacter.h"
 
-void UAnimNotify_PickMag::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+UAnimNotify_PickMag::UAnimNotify_PickMag(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
+#if WITH_EDITORONLY_DATA
+	bShouldFireInEditor = false;
+	NotifyColor = FColor(192, 183, 178);
+#endif
+}
+
+void UAnimNotify_PickMag::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	
 	if (MeshComp && MeshComp->GetOwner())
 	{
 		if (ABaseCharacter* Character = Cast<ABaseCharacter>(MeshComp->GetOwner()))

75bf8c2dbb6a456dada2f7eee3a58bcd57b4e882
Danial Kama <dankam77.dk@gmail.com>
2023-04-10 17:45:32 +0330

    Modify UAnimNotify_StandUp

--- Source/ThirdPersonShooter/Private/Animations/AnimNotify_StandUp.cpp
+++ Source/ThirdPersonShooter/Private/Animations/AnimNotify_StandUp.cpp
@@ -1,10 +1,21 @@
 // Copyright 2022-2023 Danial Kamali. All Rights Reserved.
 
 #include "AnimNotify_StandUp.h"
+
 #include "Characters/BaseCharacter.h"
 
-void UAnimNotify_StandUp::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+UAnimNotify_StandUp::UAnimNotify_StandUp(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
+#if WITH_EDITORONLY_DATA
+	bShouldFireInEditor = false;
+	NotifyColor = FColor(171, 159, 158);
+#endif
+}
+
+void UAnimNotify_StandUp::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+	
 	if (MeshComp && MeshComp->GetOwner())
 	{
 		if (const ABaseCharacter* Character = Cast<ABaseCharacter>(MeshComp->GetOwner()))

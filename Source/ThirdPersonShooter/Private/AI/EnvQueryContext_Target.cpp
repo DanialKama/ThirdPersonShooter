010f274b106d7f75dc73578b7eded545f06f6ec7
Danial Kama <dankam77.dk@gmail.com>
2023-04-24 19:05:11 +0330

    Implement EnvQueryContext_Target

---
+++ Source/ThirdPersonShooter/Private/AI/EnvQueryContext_Target.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright 2022-2023 Danial Kamali. All Rights Reserved.
+
+#include "EnvQueryContext_Target.h"
+
+#include "AIModule/Classes/AIController.h"
+#include "AIModule/Classes/EnvironmentQuery/EnvQueryTypes.h"
+#include "AIModule/Classes/EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/Pawn.h"
+
+void UEnvQueryContext_Target::ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const
+{
+	UObject* QuerierObject = QueryInstance.Owner.Get();
+	AActor* QuerierActor = Cast<AActor>(QuerierObject);
+	const APawn* AsPawn = Cast<APawn>(QuerierActor);
+	const AAIController* AIController = Cast<AAIController>(AsPawn->GetController());
+
+	UEnvQueryItemType_Actor::SetContextHelper(ContextData, AIController->GetFocusActor());
+}
---
+++ Source/ThirdPersonShooter/Private/AI/EnvQueryContext_Target.h
@@ -0,0 +1,15 @@
+﻿// Copyright 2022-2023 Danial Kamali. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIModule/Classes/EnvironmentQuery/EnvQueryContext.h"
+#include "EnvQueryContext_Target.generated.h"
+
+UCLASS(MinimalAPI)
+class  UEnvQueryContext_Target : public UEnvQueryContext
+{
+	GENERATED_BODY()
+
+	virtual void ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const override;
+};

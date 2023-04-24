785a58b5b90868b9f3193cf88432e2b0dfb7bb68
Danial Kama <dankam77.dk@gmail.com>
2023-04-24 19:05:35 +0330

    Implement UEnvQueryContext_Cover

---
+++ Source/ThirdPersonShooter/Private/AI/EnvQueryContext_Cover.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright 2022-2023 Danial Kamali. All Rights Reserved.
+
+#include "EnvQueryContext_Cover.h"
+
+#include "AIModule/Classes/EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+#include "Cover.h"
+#include "Kismet/GameplayStatics.h"
+
+void UEnvQueryContext_Cover::ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const
+{
+	TArray<AActor*> ActorSet;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACover::StaticClass(), ActorSet);
+
+	UEnvQueryItemType_Actor::SetContextHelper(ContextData, ActorSet);
+}

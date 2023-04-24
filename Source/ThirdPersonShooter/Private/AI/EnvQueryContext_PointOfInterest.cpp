d3f9ace766a29a8db067c0e8391843d8175539e1
Danial Kama <dankam77.dk@gmail.com>
2023-04-24 19:05:24 +0330

    Implement UEnvQueryContext_PointOfInterest

---
+++ Source/ThirdPersonShooter/Private/AI/EnvQueryContext_PointOfInterest.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright 2022-2023 Danial Kamali. All Rights Reserved.
+
+#include "EnvQueryContext_PointOfInterest.h"
+
+#include "AIModule/Classes/EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+#include "Kismet/GameplayStatics.h"
+#include "PointOfInterest.h"
+
+void UEnvQueryContext_PointOfInterest::ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const
+{
+	TArray<AActor*> ActorSet;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), APointOfInterest::StaticClass(), ActorSet);
+
+	UEnvQueryItemType_Actor::SetContextHelper(ContextData, ActorSet);
+}
---
+++ Source/ThirdPersonShooter/Private/AI/EnvQueryContext_PointOfInterest.h
@@ -0,0 +1,15 @@
+﻿// Copyright 2022-2023 Danial Kamali. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIModule/Classes/EnvironmentQuery/EnvQueryContext.h"
+#include "EnvQueryContext_PointOfInterest.generated.h"
+
+UCLASS(MinimalAPI)
+class UEnvQueryContext_PointOfInterest : public UEnvQueryContext
+{
+	GENERATED_BODY()
+
+	virtual void ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const override;
+};

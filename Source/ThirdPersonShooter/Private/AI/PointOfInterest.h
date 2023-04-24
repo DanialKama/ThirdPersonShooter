115c63a99d292794ba5f10e7103c415e6fc8a674
Danial Kama <dankam77.dk@gmail.com>
2023-04-24 19:04:20 +0330

    Create point of interest class

---
+++ Source/ThirdPersonShooter/Private/AI/PointOfInterest.h
@@ -0,0 +1,23 @@
+﻿// Copyright 2022-2023 Danial Kamali. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PointOfInterest.generated.h"
+
+/**
+ * Used to specify the best points on the map that AI can move to
+ */
+UCLASS()
+class APointOfInterest : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	TObjectPtr<USceneComponent> SceneComp;
+
+public:
+	/** Sets default values for this actor's properties */
+	APointOfInterest();
+};

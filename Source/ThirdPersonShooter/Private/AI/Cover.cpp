b40403e89d548f28450388ffaad75aacf3184aa7
Danial Kama <dankam77.dk@gmail.com>
2023-04-24 19:04:33 +0330

    Create cover class

---
+++ Source/ThirdPersonShooter/Private/AI/Cover.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright 2022-2023 Danial Kamali. All Rights Reserved.
+
+#include "Cover.h"
+
+ACover::ACover()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	SetHidden(true);
+	SetCanBeDamaged(false);
+
+	SceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("Scene Component"));
+	RootComponent = SceneComp;
+	SceneComp->Mobility = EComponentMobility::Static;
+}
---
+++ Source/ThirdPersonShooter/Private/AI/Cover.h
@@ -0,0 +1,23 @@
+﻿// Copyright 2022-2023 Danial Kamali. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Cover.generated.h"
+
+/**
+ * Used to specify places that AI character can take cover
+ */
+UCLASS()
+class ACover : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	TObjectPtr<USceneComponent> SceneComp;
+
+public:
+	/** Sets default values for this actor's properties */
+	ACover();
+};

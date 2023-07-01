d92850ea5ac9ecae5c4b434fce1feec278010378
Danial Kama <dankam77.dk@gmail.com>
2023-07-01 17:05:58 +0330

    Create gameplay tools class
    A set of tools to handle gameplay-related tasks

---
+++ Source/ThirdPersonShooter/Private/Tools/GameplayTools.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright 2022-2023 Danial Kamali. All Rights Reserved.
+
+#include "GameplayTools.h"
+
+void GameplayTools::SetFlag(int32& Target, const int Flag)
+{
+	Target |= Flag;
+}
+
+void GameplayTools::UnsetFlag(int32& Target, const int Flag)
+{
+	Target &= ~Flag;
+}
+
+bool GameplayTools::HasFlag(const int32 Target, const int Flag)
+{
+	return (Target & Flag) == Flag;
+}
---
+++ Source/ThirdPersonShooter/Private/Tools/GameplayTools.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2022-2023 Danial Kamali. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * A set of tools to handle gameplay-related tasks
+ */
+class GameplayTools
+{
+public:
+ static void SetFlag(int32& Target, const int Flag);
+ static void UnsetFlag(int32& Target, const int Flag);
+ static bool HasFlag(const int32 Target, const int Flag);
+};
